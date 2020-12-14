c21b9a7af8ced0f667b7373566924fffb14cea3f
Euclécio Josias Rodrigues <eucjosias@gmail.com>
2020-12-14 16:21:23 -0300

    fix: single quote to avoid error on container exec

--- scripts/exec.sh
+++ scripts/exec.sh
@@ -4,11 +4,12 @@ if [[ -z "$DBNAME" ]]; then
     printf "You must set envvar DBNAME\n"
     exit 1
 fi
-if [[ -z "$1"  ]]; then
+if [[ -z "$1" ]]; then
     printf "You must give an MQL as the first argument\n"
     exit 1
 fi
-mql=$1
+# Replace single quotes (') by double quotes (") to avoid error on container exec bash command
+mql=$(echo "$1" | sed -e "s/'/\"/g")
 
 # Get MongoDB Container ID
 mongoContainerID=$(docker ps --format "{{.ID}} {{.Image}}" | grep mongo | cut -d ' ' -f1)
